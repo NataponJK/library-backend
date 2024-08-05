f7ac4615ad6e6c36854a15721a8bf14c307c62fb
NataponJ <nataponj@gmail.com>
2024-08-05 12:19:20 +0700

    finished GET ById

--- index.js
+++ index.js
@@ -27,7 +27,21 @@ app.get('/books', async (request, response) => {
     }
 })
 
-app.post('/books', async (request, response) =>{
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+})
+
+
+
+app.post('/books', async (request, response) => {
     try {
         if(
             !request.body.title ||
